970c20eac581f932a543c86aab23afcbc9f04e77
Matthew D Jones <matthew.d.jones@tessella.com>
2018-06-01 12:51:53 +0100

    Update recipe

--- conanfile.py
+++ conanfile.py
@@ -1,21 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from conans import ConanFile
 import os
 
 class ConcurrentQueue(ConanFile):
     name = "concurrentqueue"
-    url = 'https://github.com/Manu343726/concurrentqueue'
+    url = 'https://github.com/cameron314/concurrentqueue'
     description='A fast multi-producer, multi-consumer lock-free queue for C++11'
     license = "Simplified BSD/Boost Software License"
-    version = "1.0.0"
+    version = "8f7e861"
     exports = "*.h"
-    build_policy = "missing"
-    generators = 'cmake'
+    source_subfolder = "source_subfolder"
+
+    def source(self):
+        self.run("git clone " + self.url + ".git")
+        self.run("cd " + self.name + " && git checkout " + self.version + " && cd ..")
+        os.rename(self.name, self.source_subfolder)
+
+    def build(self):
+        pass # silence warning
 
     def package(self):
         include_dir  = os.path.join('include', 'concurrentqueue')
-        include_dir2 = os.path.join('include', 'moodycamel')
 
         self.copy('concurrentqueue.h', dst=include_dir)
         self.copy('blockingconcurrentqueue.h', dst=include_dir)
-        self.copy('concurrentqueue.h', dst=include_dir2)
-self.copy('blockingconcurrentqueue.h', dst=include_dir2)
+        self.copy("LICENSE.md", dst="license", src=self.source_subfolder)
+        
+    def package_id(self):
+        self.info.header_only()
